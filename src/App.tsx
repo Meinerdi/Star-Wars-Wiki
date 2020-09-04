7c1703190003cf6706fde1a9dd9c91df830e2a3b
Meinerdi <meinerdi@gmail.com>
2020-09-04 18:54:20 +0300

    Connected with storybook

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,24 @@
 import React from 'react'
 import s from './App.module.scss'
 import Header from './components/Header/Header'
-import Sidebar from './components/Sidebar/Sidebar'
+// import Sidebar from './components/Sidebar/Sidebar'
+import { MainSidebar } from './stories/Sidebar.stories'
+
+const menuPages = [
+  'People',
+  'Species',
+  'Vehicles',
+  'Starships',
+  'Planets',
+  'Films',
+]
 
 const App = () => {
   return (
-    <div className={s["app-wrapper"]}>
+    <div className={s['app-wrapper']}>
       <div className={s['sub-wrapper']}>
         <Header />
-        <Sidebar />
+        <MainSidebar list={menuPages} navLink />
       </div>
     </div>
   )
---
+++ src/stories/Sidebar.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+import s from './Sidebar.module.scss'
+
+export interface SidebarProps {
+  list: string[]
+  navLink: boolean
+}
+
+export const Sidebar: React.FC<SidebarProps> = ({
+  list = ['First item', 'Second item', 'Third item'],
+  navLink = false,
+}) => {
+  return (
+    <aside className={s.sidebar}>
+      <nav className={s.navigation}>
+        <ul>
+          {list.map((page) => (
+            <li key={page} className={s['menu-item']}>
+              {navLink ? (
+                <NavLink
+                  to={`/${page.toLowerCase()}`}
+                  className={s['menu-link']}
+                  activeClassName={s.active}
+                >
+                  {page}
+                </NavLink>
+              ) : (
+                <a href={`/${page.toLowerCase()}`} className={s['menu-link']}>
+                  {page}
+                </a>
+              )}
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </aside>
+  )
+}
