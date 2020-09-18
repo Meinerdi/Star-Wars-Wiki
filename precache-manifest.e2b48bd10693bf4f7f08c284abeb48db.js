682bcc7f7d7957f286914ea72372c8cd51c31ae0
Meinerdi <meinerdi@gmail.com>
2020-09-18 17:48:58 +0300

    Updates

---
+++ precache-manifest.e2b48bd10693bf4f7f08c284abeb48db.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a021422515c91ae9f97e5c8aab05203a",
+    "url": "/Star-Wars-Wiki/index.html"
+  },
+  {
+    "revision": "3d55cfc34a42e535165b",
+    "url": "/Star-Wars-Wiki/static/css/main.a50d654a.chunk.css"
+  },
+  {
+    "revision": "4403e9e40ad6199caa0a",
+    "url": "/Star-Wars-Wiki/static/js/2.d3334d2e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Star-Wars-Wiki/static/js/2.d3334d2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d55cfc34a42e535165b",
+    "url": "/Star-Wars-Wiki/static/js/main.558cea65.chunk.js"
+  },
+  {
+    "revision": "a98a34a242023a52ac03",
+    "url": "/Star-Wars-Wiki/static/js/runtime-main.21fc3d71.js"
+  },
+  {
+    "revision": "75b669ad2051b20970b22acdc9891fb1",
+    "url": "/Star-Wars-Wiki/static/media/404.75b669ad.png"
+  },
+  {
+    "revision": "9fa52e3cdf2601b3168ea189543109f9",
+    "url": "/Star-Wars-Wiki/static/media/background.9fa52e3c.jpg"
+  },
+  {
+    "revision": "c5e38a8dfa8401ff2c9de9d4fda61c4f",
+    "url": "/Star-Wars-Wiki/static/media/logo.c5e38a8d.png"
+  }
+]);
